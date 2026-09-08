97a89da274e43ba194157f11843f078a8e13e398
ElenaDobrovolskaia <lenaonmars@gmail.com>
2026-09-08 16:45:14 -0700

    Chain of requests

--- locust_files/star_wars_e2e.py
+++ locust_files/star_wars_e2e.py
@@ -17,13 +17,33 @@ class PersonDataGetter(TaskSet):
                 "starship": test_data['starship'],
                 "starship_id": test_data['starship_id']}
 
-        test_name = "Data for " + test_data['person']
+        test_name = "Data for " + test_data['film']
 
-        with self.client.get("/planets", catch_response=True, name=test_name, data=data) as response:
-            if response.status_code == 200 and test_data['planet'] in response.text:
+        with self.client.get("/films/"+test_data['film_id'], catch_response=True, name=test_name, data=data) as response:
+            if response.status_code == 200 and test_data['film'] in response.text:
                 response.success()
             else:
                 response.failure("Not Found")
+
+            with self.client.get("/planets/" + test_data['planet_id'], catch_response=True,name=test_name+"_"+test_data['planet'], data=data) as sequence_response_planet:
+                if sequence_response_planet.status_code == 200 and test_data['planet'] in sequence_response_planet.text:
+                    sequence_response_planet.success()
+                else:
+                    sequence_response_planet.failure("Not Found")
+
+                with self.client.get("/people/" + test_data['person_id'], catch_response=True, name=test_name+"_"+test_data['person'], data=data) as sequence_response_people:
+                    if sequence_response_people.status_code == 200 and test_data[
+                        'person'] in sequence_response_people.text:
+                        sequence_response_people.success()
+                    else:
+                        sequence_response_people.failure("Not Found")
+
+                    with self.client.get("/starships/" + test_data['starship_id'], catch_response=True,name=test_name+"_"+test_data['starship'],data=data) as sequence_response_starships:
+                        if sequence_response_starships.status_code == 200 and test_data[
+                            'starship'] in sequence_response_starships.text:
+                            sequence_response_starships.success()
+                        else:
+                            sequence_response_starships.failure("Not Found")
 
 class E2ETest(HttpUser):
     host = 'https://swapi.info/api'
